4d3f010602a63dc4dd1c4385b1a31fc3f573f0a0
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-04-29 21:53:09 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -3,92 +3,90 @@
 
 
 /**
- * add event listener on multiple elements
- */
-
-const addEventOnElements = function (elements, eventType, callback) {
-  for (let i = 0, len = elements.length; i < len; i++) {
-    elements[i].addEventListener(eventType, callback);
-  }
-}
-
-
-
-/**
- * PRELOADER
- * 
- * preloader will be visible until document load
- */
-
-const preloader = document.querySelector("[data-preloader]");
-
-window.addEventListener("load", function () {
-  preloader.classList.add("loaded");
-  document.body.classList.add("loaded");
-});
-
-
-
-/**
- * MOBILE NAVBAR
- * 
- * show the mobile navbar when click menu button
- * and hidden after click menu close button or overlay
+ * navbar toggle
  */
 
 const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-const overlay = document.querySelector("[data-overlay]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
 
-const toggleNav = function () {
+menuToggleBtn.addEventListener("click", function () {
   navbar.classList.toggle("active");
-  overlay.classList.toggle("active");
-  document.body.classList.toggle("nav-active");
-}
+  this.classList.toggle("active");
+});
 
-addEventOnElements(navTogglers, "click", toggleNav);
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    menuToggleBtn.classList.toggle("active");
+  });
+}
 
 
 
 /**
- * HEADER & BACK TOP BTN
- * 
- * active header & back top btn when window scroll down to 100px
+ * header sticky & back to top
  */
 
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
-const activeElementOnScroll = function () {
-  if (window.scrollY > 100) {
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
     header.classList.add("active");
     backTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
   }
-}
-
-window.addEventListener("scroll", activeElementOnScroll);
+});
 
 
 
 /**
- * SCROLL REVEAL
+ * search box toggle
  */
 
-const revealElements = document.querySelectorAll("[data-reveal]");
+const searchBtn = document.querySelector("[data-search-btn]");
+const searchContainer = document.querySelector("[data-search-container]");
+const searchSubmitBtn = document.querySelector("[data-search-submit-btn]");
+const searchCloseBtn = document.querySelector("[data-search-close-btn]");
 
-const revealElementOnScroll = function () {
-  for (let i = 0, len = revealElements.length; i < len; i++) {
-    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15) {
-      revealElements[i].classList.add("revealed");
-    } else {
-      revealElements[i].classList.remove("revealed");
-    }
-  }
+const searchBoxElems = [searchBtn, searchSubmitBtn, searchCloseBtn];
+
+for (let i = 0; i < searchBoxElems.length; i++) {
+  searchBoxElems[i].addEventListener("click", function () {
+    searchContainer.classList.toggle("active");
+    document.body.classList.toggle("active");
+  });
 }
 
-window.addEventListener("scroll", revealElementOnScroll);
 
-window.addEventListener("load", revealElementOnScroll);
+
+/**
+ * move cycle on scroll
+ */
+
+const deliveryBoy = document.querySelector("[data-delivery-boy]");
+
+let deliveryBoyMove = -80;
+let lastScrollPos = 0;
+
+window.addEventListener("scroll", function () {
+
+  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+
+  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+    let activeScrollPos = window.scrollY;
+
+    if (lastScrollPos < activeScrollPos) {
+      deliveryBoyMove += 1;
+    } else {
+      deliveryBoyMove -= 1;
+    }
+
+    lastScrollPos = activeScrollPos;
+    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+  }
+
+});
